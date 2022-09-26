5407f9483b42b75e3ad9c70044a6d20f6a2c4ee2
Gabriel Bento <gabrielbento299@hotmail.com>
2022-09-26 11:47:39 -0300

    criada api e consumida com axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createServer } from 'miragejs';
+
 import App from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          cretedAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
